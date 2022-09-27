1e740f236c7a906c042d5a0f3a3ee687aa0b2a31
Rcrwrate <46920034+Rcrwrate@users.noreply.github.com>
2022-09-27 21:21:13 +0800

    更新版头说明文字

--- run.py
+++ run.py
@@ -53,7 +53,7 @@ def out(fin, x):
     e.cover(fin["content"])
     msg = fin["Replies"]
     e.add_text(
-        f'''来自https://www.nmbxd1.com/t/{fin["id"]}\n版权归属原作者及X岛匿名版''', "来源声明")
+        f'''来自https://www.nmbxd1.com/t/{fin["id"]}<br />版权归属原作者及X岛匿名版<br />请勿用于违法用途，仅供学习交流使用，请在24小时内删除<br />本文档由https://github.com/Rcrwrate/Xdnmb_downer生成''', "来源声明")
     for i in msg:
         if i["img"] != "":
             e.add_text(i["content"], i["title"], [
@@ -62,7 +62,7 @@ def out(fin, x):
             e.add_text(i["content"], i["title"])
     e.finish()
     t = TXT(fin["title"])
-    t.add(f'''来自https://www.nmbxd1.com/t/{fin["id"]}\n版权归属原作者及X岛匿名版''')
+    t.add(f'''来自https://www.nmbxd1.com/t/{fin["id"]}\n版权归属原作者及X岛匿名版\n请勿用于违法用途，仅供学习交流使用，请在24小时内删除\n本文档由https://github.com/Rcrwrate/Xdnmb_downer生成''')
     t.add(fin["content"])
     for i in fin["Replies"]:
         t.add(i["content"])
